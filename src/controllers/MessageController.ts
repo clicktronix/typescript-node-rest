4e029fc106622151e73c0b324016d8143a05d433
clicktronix <s28.morgan@gmail.com>
2019-10-03 14:28:55 +0600

    Some code refactored at MessageController

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -27,14 +27,10 @@ export default class MessageController {
    * POST /messages
    */
   public async postMessage(ctx: BaseContext) {
-    const { headers, body } = ctx.request;
+    const { body } = ctx.request;
     try {
-      const token = headers.authorization.replace('Bearer ', '');
-      const decoded = jwt.verify(token, CONFIG.jwt_encryption);
-      if (!decoded || isString(decoded)) {
-        return ctx.throw(httpStatus.UNAUTHORIZED, 'Invalid token');
-      }
-      const user = await User.findById((decoded as IUserModel).id);
+      const usersMeta = this.decodeToken(ctx);
+      const user = await User.findById(usersMeta.id);
       if (!user) {
         return ctx.throw(httpStatus.NOT_FOUND, 'User not found');
       }
@@ -54,22 +50,14 @@ export default class MessageController {
    * PATCH /messages
    */
   public async updateMessage(ctx: BaseContext) {
-    const { headers, body } = ctx.request;
+    const { body } = ctx.request;
     try {
-      const token = headers.authorization.replace('Bearer ', '');
-      const decoded = jwt.verify(token, CONFIG.jwt_encryption);
-      if (!decoded || isString(decoded)) {
-        return ctx.throw(httpStatus.UNAUTHORIZED, 'Invalid token');
+      const usersMeta = this.decodeToken(ctx);
+      const message = await this.findMessage(ctx, usersMeta);
+      if (message) {
+        message.content = body.content;
+        await message.save();
       }
-      const message = await Message.findById(ctx.params.id);
-      if (!message) {
-        return ctx.throw(httpStatus.NOT_FOUND, 'Message not found');
-      }
-      if (message.user.id !== (decoded as IUserModel).id) {
-        return ctx.throw(httpStatus.UNAUTHORIZED, 'You have not permissions');
-      }
-      message.content = body.content;
-      await message.save();
       ctx.status = httpStatus.OK;
     } catch (err) {
       ctx.throw(err.status, err.message);
@@ -80,24 +68,38 @@ export default class MessageController {
    * DELETE /messages
    */
   public async deleteMessage(ctx: BaseContext) {
-    const { headers } = ctx.request;
     try {
-      const token = headers.authorization.replace('Bearer ', '');
-      const decoded = jwt.verify(token, CONFIG.jwt_encryption);
-      if (!decoded || isString(decoded)) {
-        return ctx.throw(httpStatus.UNAUTHORIZED, 'Invalid token');
-      }
-      const message = await Message.findById(ctx.params.id);
-      if (!message) {
-        return ctx.throw(httpStatus.NOT_FOUND, 'Message not found');
-      }
-      if (message.user.id !== (decoded as IUserModel).id) {
-        return ctx.throw(httpStatus.UNAUTHORIZED, 'You have not permissions');
-      }
-      await message.remove();
+      const usersMeta = this.decodeToken(ctx);
+      const message = await this.findMessage(ctx, usersMeta);
+      message && await message.remove();
       ctx.status = httpStatus.OK;
     } catch (err) {
       ctx.throw(err.status, err.message);
     }
+  }
+
+  private async findMessage(ctx: BaseContext, usersMeta: IUserModel) {
+    try {
+      const message = await Message.findById(ctx.params.id);
+      if (!message) {
+        return ctx.throw(httpStatus.NOT_FOUND, 'Message not found');
+      }
+      if (message.user.id !== usersMeta.id) {
+        return ctx.throw(httpStatus.UNAUTHORIZED, 'You have not permissions');
+      }
+      return message;
+    } catch (err) {
+      ctx.throw(err.status, err.message);
+    }
+  }
+
+  private decodeToken(ctx: BaseContext) {
+    const { headers } = ctx.request;
+    const token = headers.authorization.replace('Bearer ', '');
+    const decoded = jwt.verify(token, CONFIG.jwt_encryption);
+    if (!decoded || isString(decoded)) {
+      return ctx.throw(httpStatus.UNAUTHORIZED, 'Invalid token');
+    }
+    return decoded as IUserModel;
   }
 }
