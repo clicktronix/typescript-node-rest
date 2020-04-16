539060a03bc6a268639598253ec51d1ad22466e7
clicktronix <s28.morgan@gmail.com>
2020-04-16 23:14:53 +0600

    Deploy frontend

---
+++ src/public/precache-manifest.8687f4bccb80a68a3274354c91d2bccc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "737f9634dd6d22414b36cd0b19293ca2",
+    "url": "=https://mysterious-cove-46705.herokuapp.com/index.html"
+  },
+  {
+    "revision": "2baad3ab8a5af0b9031b",
+    "url": "=https://mysterious-cove-46705.herokuapp.com/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "fe9ad4810bf4f2a2ee3c",
+    "url": "=https://mysterious-cove-46705.herokuapp.com/static/css/main.8dcbfaa7.chunk.css"
+  },
+  {
+    "revision": "2baad3ab8a5af0b9031b",
+    "url": "=https://mysterious-cove-46705.herokuapp.com/static/js/2.f8ba9bc8.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "=https://mysterious-cove-46705.herokuapp.com/static/js/2.f8ba9bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe9ad4810bf4f2a2ee3c",
+    "url": "=https://mysterious-cove-46705.herokuapp.com/static/js/main.f4c123eb.chunk.js"
+  },
+  {
+    "revision": "ebaa890e7d623a0ff082",
+    "url": "=https://mysterious-cove-46705.herokuapp.com/static/js/runtime-main.898e6c6d.js"
+  }
+]);
