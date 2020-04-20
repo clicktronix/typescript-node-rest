f149a375fc8592934d9d3c624ad17de3efc1a3ae
clicktronix <s28.morgan@gmail.com>
2020-04-21 00:36:43 +0600

    Frontend deploy

---
+++ public/precache-manifest.5885b1bde4919c942eb1e5301416ff1b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1efd732fe9cb91d5c3e01744e77740",
+    "url": "/index.html"
+  },
+  {
+    "revision": "013eb9baa3468997a488",
+    "url": "/static/css/2.1bd2ab2f.chunk.css"
+  },
+  {
+    "revision": "5a89eca8e0adbacde7c8",
+    "url": "/static/css/main.8dcbfaa7.chunk.css"
+  },
+  {
+    "revision": "013eb9baa3468997a488",
+    "url": "/static/js/2.61db024c.chunk.js"
+  },
+  {
+    "revision": "7980feede54689de2a3db22569a91a7b",
+    "url": "/static/js/2.61db024c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a89eca8e0adbacde7c8",
+    "url": "/static/js/main.3cadff79.chunk.js"
+  },
+  {
+    "revision": "f4e7851959c9bfe00461",
+    "url": "/static/js/runtime-main.04cd5d7f.js"
+  }
+]);
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "https://mysterious-cove-46705.herokuapp.com/precache-manifest.90e67d6ffecca8789f04c6c19e562b05.js"
+  "/precache-manifest.5885b1bde4919c942eb1e5301416ff1b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://mysterious-cove-46705.herokuapp.com/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
