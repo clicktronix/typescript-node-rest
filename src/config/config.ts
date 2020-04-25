f3952fdedfdc8c125b2893401f6a1e9126f08b61
clicktronix <s28.morgan@gmail.com>
2020-04-25 19:49:52 +0600

    Imports imroved

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,7 +1,7 @@
-import * as mongoose from 'mongoose';
-import * as bcrypt from 'bcrypt';
-import * as jwt from 'jsonwebtoken';
-import * as R from 'ramda';
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import R from 'ramda';
 
 import { CONFIG } from 'config';
 
